c5e67d9daf2d58a8acceba2de997cbcb98112451
Rayan Bassetti <r.bassetti@etu-webschoolfactory.fr>
2019-02-25 09:48:48 +0100

    Basic HTML and CSS

--- home.php
+++ home.php
@@ -1,10 +1,96 @@
 <?php get_header(); ?>
 
-<section #TopBanner></section>
-<section #Presentation>
-    <div #PresentationTexte>
-        <p>Rendez-vous blablabla</p>
+<section #hometopbanner>
+    <div></div>
+    <div>Menu drop down</div>
+</section>
+<section #HomeCarrousel>
+</section>
+<section #HomePresentation>
+    <div #HomePresentationContent>
+        <h2>"Keep It Short", c'est quoi :</h2>
+        <p>
+        Rendez-vous de professionnels et étudiants passionnés de cinéma, organisé par
+        l’association étudiante les Hallucinés, le festival Keep It Short se déroulera 
+        cette année début octobre dans la belle ville de Nantes. 
+        Au programme : projection de courts-métrages étudiants, remise des prix, 
+        interludes artistiques et plaisirs gustatifs. Alors, n’attendez plus ! 
+        Si vous êtes étudiant, que vous avez un court-métrage sous la main et que vous
+        rêvez de mettre la main sur le Prix du meilleur scénario, le Prix du Jury ou 
+        le Prix du Public, envoyez-nous votre production !
+        Pour la 5e édition du festival, les Hallucinés décident de réhabiliter les 
+        courts-métrages d’animation et de remettre un prix à cette catégorie. 
+        N’hésitez pas non plus à exposer vos talents !
+
+        </p>
     </div>
+</section>
+<section #HomeInfosPratiques>
+    <a href="#" class="buttoncta">Infos pratiques</a> 
+</section>
+<section #HomeParticiper>
+    <h2>Participer : </h2>
+    <p>
+        4 prix seront décernés lors de cette 5e édition :
+        <ul>
+            <li> 
+                Le Prix du Meilleur Scénario
+            </li>
+            <li> 
+                Le Prix du Jury
+            </li>
+            <li> 
+                Le Prix du Public
+            </li>
+            <li> 
+                Le Prix du Meilleur Court-Métrage d'Animation
+            </li>
+        </ul>
+        Pas de thème imposé pour le festival Keep It Short, vous êtes donc libre de nous envoyer n’importe 
+        quel court-métrage : que vous l’ayez réalisé dans le cadre de vos études, avec votre association 
+        ou simplement pour le plaisir…
+        Quelques règles à respecter cependant :
+        <ul>
+            <li>
+                Vous devez être étudiant.
+            </li>
+            <li>
+                Votre court-métrage ne doit pas dépasser 10 (grand maximum 15) minutes.
+            </li>
+            <li>
+                Vous devez nous l'envoyez avant le 15 Septembre.
+            </li>
+        </ul>
+        Vous devez être étudiant
+        Votre court-métrage ne doit pas dépasser 10mn (grand maximum 15mn)
+        Vous devez nous l’envoyer avant le 5 septembre
+        Pour nous contacter, une seule adresse :
+    </p>
+    <a href="#" class="buttoncta">Participer</a>
+</section>
+<section #HomeEdition2018>
+
+</section>
+
+<section #HomePartenaires>
+    <h2>Devenir Partenaire Keep It Short</h2>
+    <p>
+    Prenez part à un événement dynamique, concernant tous les étudiants de France et la population nantaise.
+    </p>
+    <a href="#" class="buttoncta">Devenir partenaire</a>
+</section>
+<section #HomeSocialNetworks>
+    <ul>
+        <li>
+            Facebook
+        </li>
+        <li>
+            Twitter
+        </li>
+        <li>
+            Instagram
+        </li>
+    </ul>
 </section>
 
 </body>
