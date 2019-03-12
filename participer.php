1db4a205eb93f8f0040d286db6891658bf6fc28a
Rayan Bassetti <r.bassetti@etu-webschoolfactory.fr>
2019-03-12 20:32:49 +0700

    Added new page + Typeform

---
+++ participer.php
@@ -0,0 +1,26 @@
+<?php
+/* Template Name: Participer */
+?>
+
+
+<?php get_header(); ?>
+
+<section id="Participer">
+    <div id="ParticiperContent">
+    <a class="typeform-share button" href="https://rayanbassetti.typeform.com/to/nVdsgK" data-mode="popup" 
+        target="_blank">Participer 
+    </a> 
+        
+        <script> (function() {
+             var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm_share", b="https://embed.typeform.com/"; 
+             if(!gi.call(d,id)){
+                  js=ce.call(d,"script"); 
+                  js.id=id; js.src=b+"embed.js"; 
+                  q=gt.call(d,"script")[0]; 
+                  q.parentNode.insertBefore(js,q) 
+                  } 
+                })() 
+        </script>
+</section>
+
+<?php get_footer(); ?>
