0f4b4675e085f25247cd3bc79543c4c8c6f53b3e
Rayan Bassetti <r.bassetti@etu-webschoolfactory.fr>
2019-03-25 00:36:02 +0700

    Fixed functions.php

--- functions.php
+++ functions.php
@@ -5,13 +5,9 @@
  * @package MyFirstTheme
  * @since MyFirstTheme 1.0
  */
-?>
-
-<?php 
-
 add_action( 'wp_enqueue_scripts', 'theme_name_scripts' );
 function theme_name_scripts() {
-   wp_enqueue_style( 'style-name', get_stylesheet_uri() . "/css/style.css" );
+   wp_enqueue_style( 'style', get_stylesheet_uri() . "./style.css" );
 }
 
 ?>
